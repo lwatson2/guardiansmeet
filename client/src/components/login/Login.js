1fb2e89ce63be737db304c8bb62a820e0b3c1b3f
lwatson2 <watson.logan@yahoo.com>
2019-09-25 22:18:02 -0500

    continued styling the user profile

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React from "react";
 import styled from "styled-components";
 import useForm from "../helpers/FormHelper";
 import { device } from "../helpers/mediaQueries";
